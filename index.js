9e1fdd9d17ebd2d363772854e5e154293c72d05c
realmayus <realmayus@gmail.com>
2020-08-14 12:54:09 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 8080
+const apiUrl = "https://api.sendgrid.com/v3"
+const secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'))
+const {sendgridKey, destinationEmailAddress} = secrets
+
+
+app.post("/post", async(ctx) => {
+    let email = await ctx.body();
+    let res = await fetch(apiUrl + "/mail/send", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + sendgridKey
+        },
+        body: JSON.stringify({
+            personalizations: [
+                {
+                    subject: email.subject,
+                    to: [{
+                        email: destinationEmailAddress
+                    }]
+                }
+            ],
+            from: {
+                email: "noreply@realmayus.xyz",
+                name: "Email Proxy: " + email.from
+            },
+            reply_to: {
+                email: JSON.parse(email.envelope).from
+            },
+            content: [
+                {
+                    type: "text/html",
+                    value: email.text
+                }
+            ]
+        })
+    })
+
+    res = await res.text()
+    return res;
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)})
